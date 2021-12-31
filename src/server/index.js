24972519cc7bf6dc30be8a8f2692cfee7a4e09f5
Okan Kayas <okan-kayas@web.de>
2021-12-31 15:33:21 +0100

    Cleaned up client.js

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ app.get('/apod', async (req, res) => {
     const image = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
     ).then((res) => res.json())
-    res.send({ image })
+    res.send(image)
   } catch (err) {
     console.log('error:', err)
   }
